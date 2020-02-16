ab53298f81c014c77025391e0bbb20b821e77e74
alpden550 <alpden550@gmail.com>
2020-02-16 13:27:23 +0300

    Added model Order.

--- food_delivery/models.py
+++ food_delivery/models.py
@@ -1,8 +1,14 @@
 from sqlalchemy import event
-from sqlalchemy_utils import EmailType
+from sqlalchemy_utils import ChoiceType, EmailType, PhoneNumber
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from food_delivery.extensions import db
+
+orders_meals = db.Table(
+    'orders_meals',
+    db.Column('meal_id', db.Integer, db.ForeignKey('meal.id'), primary_key=True),
+    db.Column('order_id', db.Integer, db.ForeignKey('order.id'), primary_key=True),
+)
 
 
 class User(db.Model):
@@ -36,11 +42,38 @@ class Meal(db.Model):
     price = db.Column(db.Float())
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category', back_populates='meals', lazy='joined')
+    orders = db.relationship(
+        'Order', secondary='orders_meals', back_populates='meals', lazy='joined'
+    )
 
     def __repr__(self):
         return f'<Meal {self.title}>'
 
 
+class Order(db.Model):
+    STATUS_TYPES = [
+        ('NEW', 'Новый'),
+        ('DELIVERING', 'Выполняется'),
+        ('DONE', 'Готово'),
+    ]
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    amount = db.Column(db.Integer)
+    status = db.Column(ChoiceType(STATUS_TYPES), default='NEW')
+    client_name = db.Column(db.String(25), nullable=False)
+    client_address = db.Column(db.String(250), nullable=False)
+    client_email = db.Column(EmailType, nullable=False)
+    client_phone = db.Column(db.String)
+    meals = db.relationship(
+        'Meal', secondary='orders_meals', back_populates='orders', lazy='joined'
+    )
+
+
 @event.listens_for(User.password, 'set', retval=True)
 def hash_user_password(target, value, *args):  # noqa:WPS110
     return generate_password_hash(value)
+
+
+@event.listens_for(Order.client_phone, 'set', retval=True)
+def format_phone(turget, number, *args):
+    return PhoneNumber(number, region='RU').international
