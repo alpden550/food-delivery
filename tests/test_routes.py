3e1e4e00a5954fd813ccf752023472327c09fb78
alpden550 <alpden550@gmail.com>
2020-02-21 13:48:33 +0300

    Added test for session, add item to cart, delete item from cart, test login pages.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,69 +1,130 @@
 import pytest
+from flask import session
+from flask_login import current_user
 
 from food_delivery import create_app
+from food_delivery.extensions import db
+from food_delivery.models import Meal, User
 
-app = create_app()
+app = create_app(config_name='testing')
+db.create_all(app=app)
 
 
 @pytest.fixture
-def flask_app():
-    app.testing = True
-    client = app.test_client()
-    yield client
+def client():
+    with app.test_client() as client:
+        yield client
 
 
 class TestMainRoutes:
 
-    def test_index(self, flask_app):
-        response = flask_app.get('/')
+    @classmethod
+    def setup_class(cls):
+        with app.app_context():
+            meal = Meal(title='Meal')
+            db.session.add(meal)
+            db.session.commit()
+
+    @classmethod
+    def teardown_class(cls):
+        with app.app_context():
+            meal = Meal.query.first()
+            db.session.delete(meal)
+            db.session.commit()
+
+    def test_index_page(self, client):
+        response = client.get('/')
         assert response.status_code == 200
 
-    def test_cart_redirect(self, flask_app):
-        response = flask_app.get('/cart')
+    def test_cart_redirect(self, client):
+        response = client.get('/cart')
         assert response.status_code == 308
 
-    def test_cart(self, flask_app):
-        response = flask_app.get('/cart/')
+    def test_cart_page(self, client):
+        response = client.get('/cart/')
         assert response.status_code == 200
 
-    def test_account(self, flask_app):
-        response = flask_app.get('/account')
+    def test_account_page(self, client):
+        response = client.get('/account')
         assert response.status_code == 302
 
-    def test_wrong_account(self, flask_app):
-        response = flask_app.get('/account/')
+    def test_wrong_account(self, client):
+        response = client.get('/account/')
         assert response.status_code == 404
 
-    def test_add_to_cart(self, flask_app):
-        response = flask_app.get('/addtocart/1')
+    def test_add_to_cart_page(self, client):
+        response = client.get('/addtocart/1')
         assert response.status_code == 302
 
-    def teat_remove_from_cart(self, flask_app):
-        response = flask_app.get('/delete/1')
+    def teat_remove_from_cart_page(self, client):
+        response = client.get('/delete/1')
         assert response.status_code == 302
 
-    def test_ordered(self, flask_app):
-        response = flask_app.get('/ordered')
+    def test_ordered_page(self, client):
+        response = client.get('/ordered')
         assert response.status_code == 200
+
+    def test_add_to_cart_item(self, client):
+        client.get('/addtocart/1')
+        assert session.get('cart') == [1]
+
+    def test_delete_item_from_cart(self, client):
+        client.get('/addtocart/1')
+        client.get('/delete/1')
+        assert session.get('cart') == []
+
+    def test_empty_cart(self, client):
+        client.get('/')
+        assert session.get('cart') is None
 
 
 class TestAuthRoutes:
-    def test_admin_redirect(self, flask_app):
-        response = flask_app.get('/admin')
+
+    @classmethod
+    def setup_class(cls):
+        with app.app_context():
+            admin = User(username='admin', is_admin=True, email='email', password='password')
+            db.session.add(admin)
+            db.session.commit()
+
+    @classmethod
+    def teardown_class(cls):
+        with app.app_context():
+            admin = User.query.first()
+            db.session.delete(admin)
+            db.session.commit()
+
+    def test_admin_redirect(self, client):
+        response = client.get('/admin')
         assert response.status_code == 308
 
-    def test_admin(self, flask_app):
-        response = flask_app.get('/admin/')
+    def test_admin_page(self, client):
+        response = client.get('/admin/')
         assert response.status_code == 302
 
-    def test_login(self, flask_app):
-        response = flask_app.get('/auth/login/')
+    def test_login_page(self, client):
+        response = client.get('/auth/login/')
         assert response.status_code == 200
 
-    def test_registration(self, flask_app):
-        response = flask_app.get('/auth/registration/')
+    def test_registration_page(self, client):
+        response = client.get('/auth/registration/')
         assert response.status_code == 200
 
-    def test_logout(self, flask_app):
-        response = flask_app.get('/auth/logout/')
+    def test_logout(self, client):
+        response = client.get('/auth/logout/')
         assert response.status_code == 302
+
+    def test_user_can_admin(self, client):
+        client.post(
+            '/auth/login/',
+            data={
+                'email': 'email',
+                'password': 'password'},
+            follow_redirects=True)
+        response = client.get('/admin')
+        assert response.location == 'http://localhost/admin/'
+
+    def test_user_can_login(self, client):
+        client.post('/auth/login/', data={'email': 'email', 'password': 'password'}, follow_redirects=True)
+        response = client.get('/account')
+        assert response.status_code == 200
